8dc52c3e436d04f34d32086a20fc3867b5f86f52
cgcladera-vancast <carles@vancast.net>
2013-05-03 11:29:21 +0200

    Progress trigger and improved error event object

--- examples/simple.js
+++ examples/simple.js
@@ -1,14 +1,14 @@
 var AB = require('../index.js');
 
 var test = new AB({
-	iterations: 1,
+	iterations: 2,
 	delay: 0,
 	requests: 2000,
 	concurrency: 20,
 	host: 'http://pulldev.webcasting-studio.net/?s=questions&part_id=588'
 });
 test.on('error', function (error){
-	console.log('Test error: '+error);
+	console.log(error);
 });
 test.on('progress', function (progress){
 	console.log(progress);
