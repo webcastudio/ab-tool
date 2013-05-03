d9ab01bc2b6d9545287c3780920445724383f428
cgcladera-vancast <carles@vancast.net>
2013-05-03 11:09:36 +0200

    Iteration progress trigger

--- examples/simple.js
+++ examples/simple.js
@@ -3,12 +3,15 @@ var AB = require('../index.js');
 var test = new AB({
 	iterations: 1,
 	delay: 0,
-	requests: 1,
-	concurrency: 1,
-	host: 'http://pulldev.webcasting-studio.net/'
+	requests: 2000,
+	concurrency: 20,
+	host: 'http://pulldev.webcasting-studio.net/?s=questions&part_id=588'
 });
 test.on('error', function (error){
 	console.log('Test error: '+error);
+});
+test.on('progress', function (progress){
+	console.log(progress);
 });
 test.on('iteration', function (result){
 	console.log(result);
