7176ffc59d30f69f9f69f6dbb9c3679a4037af8c
cgcladera-vancast <carles@vancast.net>
2013-05-03 09:21:33 +0200

    Handle and emit errors

--- examples/simple.js
+++ examples/simple.js
@@ -1,11 +1,14 @@
 var AB = require('../index.js');
 
 var test = new AB({
-	iterations: 10,
-	delay: 10000,
-	requests: 10,
-	concurrency: 2,
+	iterations: 1,
+	delay: 0,
+	requests: 1,
+	concurrency: 1,
 	host: 'http://www.carlosgalancladera.net/'
+});
+test.on('error', function (error){
+	console.log('Test error: '+error);
 });
 test.on('iteration', function (result){
 	console.log(result);
