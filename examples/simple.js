ec75eca963969a263b5972cf227f4999cf611abd
cgcladera <cgcladera@gmail.com>
2014-03-21 11:56:16 +0100

    Adds headers example

--- examples/simple.js
+++ examples/simple.js
@@ -1,20 +1,23 @@
 var AB = require('../index.js');
 
 var test = new AB({
-	iterations: 2,
+	iterations: 1,
 	delay: 0,
-	requests: 2000,
-	concurrency: 20,
-	host: 'http://www.carlosgalancladera.net/'
+	requests: 10,
+	concurrency: 5,
+	host: 'http://carlosgalancladera.net/',
+  headers: {
+    'Custom-header': 'custom'
+  }
 });
 test.on('error', function (error){
-	console.log(error);
+	console.log('Error', error);
 });
 test.on('progress', function (progress){
-	console.log(progress);
+	console.log('Progress', progress);
 });
 test.on('iteration', function (result){
-	console.log(result);
+	console.log('Iteration',result);
 });
 test.on('finish', function (){
 	console.log("Test finished");
